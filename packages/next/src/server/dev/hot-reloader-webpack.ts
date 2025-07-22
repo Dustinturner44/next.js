39e017d8acdb755e59c67c3e4070d97ca8d270bf
Tim Neutkens <tim@timneutkens.nl>
2025-07-22 17:29:47 +0200

    Routing: Remove only place isApp is passed to ensurePage

--- packages/next/src/server/dev/hot-reloader-webpack.ts
+++ packages/next/src/server/dev/hot-reloader-webpack.ts
@@ -1667,13 +1667,11 @@ export default class HotReloaderWebpack implements NextJsHotReloaderInterface {
     clientOnly,
     appPaths,
     definition,
-    isApp,
     url,
   }: {
     page: string
     clientOnly: boolean
     appPaths?: ReadonlyArray<string> | null
-    isApp?: boolean
     definition?: RouteDefinition
     url?: string
   }): Promise<void> {
@@ -1697,7 +1695,6 @@ export default class HotReloaderWebpack implements NextJsHotReloaderInterface {
           page,
           appPaths,
           definition,
-          isApp,
           url,
         })
       })
