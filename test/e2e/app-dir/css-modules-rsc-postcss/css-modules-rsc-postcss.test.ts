756221bb2d4c0498fe1d36f1c68bf79944b4d4c0
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-08-12 16:17:26 +0200

    Turbopack: fix module rules for `.module.scss` (#82570)
    It ran `postcss(webpack(postcss(filesource)))`, which doesn't work.
    
    And changing the order does make sense, since we do want Webpack (SCSS) to run first, and only then PostCSS?
    
    #82554 didn't catch every case

--- test/e2e/app-dir/css-modules-rsc-postcss/css-modules-rsc-postcss.test.ts
+++ test/e2e/app-dir/css-modules-rsc-postcss/css-modules-rsc-postcss.test.ts
@@ -5,12 +5,16 @@ describe('css-modules-rsc-postcss', () => {
     files: __dirname,
     dependencies: {
       'postcss-nested': '4.2.1',
+      sass: 'latest',
     },
   })
 
   it('should compile successfully and apply the correct styles', async () => {
     const browser = await next.browser('/')
     expect(await browser.elementByCss('p').getComputedCss('color')).toBe(
+      'rgb(0, 128, 0)'
+    )
+    expect(await browser.elementByCss('span').getComputedCss('color')).toBe(
       'rgb(0, 128, 0)'
     )
   })
