Dustinturner44/next.js

commit 0ec5511da47c98112e86290d2f1550ad261d91b2

Turbopack: fix CSS module references, take 2 (#82448)
Replaces and closes https://github.com/vercel/next.js/pull/78058
Closes PACK-4324

Before, you got duplicate modules because
`transition_to_client(postcss(webpack(source in rsc)) != postcss(webpack(transition_to_client(source in rsc))`

Now, we have:
```
rsc layer
--ESM import->
CSSModuleAsset(in rsc, contains raw SCSS source)
--CssReferenceSubType::Inner->
CssAsset(first in client layer, then processed with Webpack loader)
```

This aligns it with when you import CSS from a client component, in both cases the CSS module is first transitioned into the client layer, and then processed with Webpack

turbopack/crates/turbopack/src/module_options/mod.rs
@@ -137,6 +137,7 @@ impl ModuleOptions {
                 CssOptionsContext {
                     enable_raw_css,
                     source_maps: css_source_maps,
+                    ref module_css_condition,
                     ..
                 },
             ref enable_postcss_transform,
@@ -148,6 +149,28 @@ impl ModuleOptions {
             keep_last_successful_parse,
             ..
         } = *module_options_context.await?;
+
+        let module_css_condition = module_css_condition.clone().unwrap_or_else(|| {
+            RuleCondition::any(vec![
+                RuleCondition::ResourcePathEndsWith(".module.css".to_string()),
+                RuleCondition::ContentTypeStartsWith("text/css+module".to_string()),
+            ])
+        });
+
+        // For React Client References, the CSS Module "facade" module lives in the parent (server)
+        // module context, but the facade's references should be transitioned to the client (and
+        // only then be processed with Webpack/PostCSS).
+        //
+        // Note that this is not an exhaustive condition for PostCSS/Webpack, but excludes certain
+        // cases, so it should be added conjunctively together with the `module_css_condition` rule.
+        //
+        // If module css, then only when (Inner or Analyze or Compose)
+        // <=> (not (module css)) or (Inner or Analyzer or Compose)
+        let module_css_external_transform_conditions = vec![
+            RuleCondition::ReferenceType(ReferenceType::Css(CssReferenceSubType::Inner)),
+            RuleCondition::ReferenceType(ReferenceType::Css(CssReferenceSubType::Analyze)),
+            RuleCondition::ReferenceType(ReferenceType::Css(CssReferenceSubType::Compose)),
+        ];
 
         let mut ts_preprocess = vec![];
         let mut ecma_preprocess = vec![];
@@ -458,10 +481,7 @@ impl ModuleOptions {
                     })],
                 ),
                 ModuleRule::new(
-                    RuleCondition::any(vec![
-                        RuleCondition::ResourcePathEndsWith(".module.css".to_string()),
-                        RuleCondition::ContentTypeStartsWith("text/css+module".to_string()),
-                    ]),
+                    module_css_condition.clone(),
                     vec![ModuleRuleEffect::ModuleType(ModuleType::Css {
                         ty: CssModuleAssetType::Module,
                         environment,
@@ -486,8 +506,21 @@ impl ModuleOptions {
 
                 rules.push(ModuleRule::new(
                     RuleCondition::Any(vec![
-                        RuleCondition::ResourcePathEndsWith(".css".to_string()),
-                        RuleCondition::ContentTypeStartsWith("text/css".to_string()),
+                        RuleCondition::All(vec![
+                            RuleCondition::Any(vec![
+                                RuleCondition::ResourcePathEndsWith(".css".to_string()),
+                                RuleCondition::ContentTypeStartsWith("text/css".to_string()),
+                            ]),
+                            RuleCondition::not(module_css_condition.clone()),
+                        ]),
+                        RuleCondition::All(
+                            [
+                                vec![module_css_condition.clone()],
+                                // see comment on module_css_external_transform_conditions
+                                module_css_external_transform_conditions.clone(),
+                            ]
+                            .concat(),
+                        ),
                     ]),
                     vec![ModuleRuleEffect::SourceTransforms(ResolvedVc::cell(vec![
                         ResolvedVc::upcast(
@@ -523,10 +556,7 @@ impl ModuleOptions {
                 ),
                 ModuleRule::new(
                     RuleCondition::all(vec![
-                        RuleCondition::Any(vec![
-                            RuleCondition::ResourcePathEndsWith(".module.css".to_string()),
-                            RuleCondition::ContentTypeStartsWith("text/css+module".to_string()),
-                        ]),
+                        module_css_condition.clone(),
                         // Only create a module CSS asset if not `@import`ed from CSS already.
                         // NOTE: `composes` references should not be treated as `@import`s and
                         // should also create a module CSS asset.
@@ -538,10 +568,7 @@ impl ModuleOptions {
                 ),
                 ModuleRule::new(
                     RuleCondition::all(vec![
-                        RuleCondition::Any(vec![
-                            RuleCondition::ResourcePathEndsWith(".module.css".to_string()),
-                            RuleCondition::ContentTypeStartsWith("text/css+module".to_string()),
-                        ]),
+                        module_css_condition.clone(),
                         // Create a normal CSS asset if `@import`ed from CSS already.
                         RuleCondition::ReferenceType(ReferenceType::Css(
                             CssReferenceSubType::AtImport(None),
@@ -553,10 +580,12 @@ impl ModuleOptions {
                     })],
                 ),
                 // Ecmascript CSS Modules referencing the actual CSS module to include it
-                ModuleRule::new_internal(
-                    RuleCondition::Any(vec![
-                        RuleCondition::ResourcePathEndsWith(".module.css".to_string()),
-                        RuleCondition::ContentTypeStartsWith("text/css+module".to_string()),
+                ModuleRule::new(
+                    RuleCondition::all(vec![
+                        RuleCondition::ReferenceType(ReferenceType::Css(
+                            CssReferenceSubType::Inner,
+                        )),
+                        module_css_condition.clone(),
                     ]),
                     vec![ModuleRuleEffect::ModuleType(ModuleType::Css {
                         ty: CssModuleAssetType::Module,
@@ -569,10 +598,7 @@ impl ModuleOptions {
                         RuleCondition::ReferenceType(ReferenceType::Css(
                             CssReferenceSubType::Analyze,
                         )),
-                        RuleCondition::Any(vec![
-                            RuleCondition::ResourcePathEndsWith(".module.css".to_string()),
-                            RuleCondition::ContentTypeStartsWith("text/css+module".to_string()),
-                        ]),
+                        module_css_condition.clone(),
                     ]),
                     vec![ModuleRuleEffect::ModuleType(ModuleType::Css {
                         ty: CssModuleAssetType::Module,
@@ -677,6 +703,14 @@ impl ModuleOptions {
                             RuleCondition::ResourceBasePathGlob(Glob::new(key.clone()).await?)
                         },
                         RuleCondition::not(RuleCondition::ResourceIsVirtualSource),
+                        // see comment on module_css_external_transform_conditions
+                        RuleCondition::Any(
+                            [
+                                vec![RuleCondition::not(module_css_condition.clone())],
+                                module_css_external_transform_conditions.clone(),
+                            ]
+                            .concat(),
+                        ),
                     ]),
                     vec![ModuleRuleEffect::SourceTransforms(ResolvedVc::cell(vec![
                         ResolvedVc::upcast(

turbopack/crates/turbo-tasks/src/vc/mod.rs
@@ -357,7 +357,7 @@ where
     T: ?Sized,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Vc").field("node", &self.node).finish()
+        f.debug_tuple("Vc").field(&self.node).finish()
     }
 }
 

turbopack/crates/turbopack-core/src/module_graph/mod.rs
@@ -348,8 +348,6 @@ impl SingleModuleGraph {
         #[cfg(debug_assertions)]
         {
             use once_cell::sync::Lazy;
-
-            // TODO(PACK-4578): This is temporary while the last issues are being addressed.
             static CHECK_FOR_DUPLICATE_MODULES: Lazy<bool> = Lazy::new(|| {
                 match std::env::var_os("TURBOPACK_TEMP_DISABLE_DUPLICATE_MODULES_CHECK") {
                     Some(v) => v != "1" && v != "true",

.github/workflows/build_and_test.yml
@@ -314,8 +314,6 @@ jobs:
         export TURBOPACK_BUILD=1
         export NEXT_TEST_MODE=start
         export NEXT_TEST_REACT_VERSION="${{ matrix.react }}"
-        # TODO(PACK-4578): Remove
-        export TURBOPACK_TEMP_DISABLE_DUPLICATE_MODULES_CHECK=1
 
         node run-tests.js --timings -g ${{ matrix.group }} --type production
       stepName: 'test-turbopack-production-react-${{ matrix.react }}-${{ matrix.group }}'

crates/next-api/src/app.rs
@@ -34,7 +34,7 @@ use next_core::{
     },
     next_server_utility::{NEXT_SERVER_UTILITY_MERGE_TAG, NextServerUtilityTransition},
     parse_segment_config_from_source,
-    util::NextRuntime,
+    util::{NextRuntime, module_styles_rule_condition, styles_rule_condition},
 };
 use serde::{Deserialize, Serialize};
 use tracing::Instrument;
@@ -99,37 +99,6 @@ pub struct AppProject {
 pub struct OptionAppProject(Option<ResolvedVc<AppProject>>);
 
 impl AppProject {}
-
-fn styles_rule_condition() -> RuleCondition {
-    RuleCondition::any(vec![
-        RuleCondition::all(vec![
-            RuleCondition::ResourcePathEndsWith(".css".into()),
-            RuleCondition::not(RuleCondition::ResourcePathEndsWith(".module.css".into())),
-        ]),
-        RuleCondition::all(vec![
-            RuleCondition::ResourcePathEndsWith(".scss".into()),
-            RuleCondition::not(RuleCondition::ResourcePathEndsWith(".module.scss".into())),
-        ]),
-        RuleCondition::all(vec![
-            RuleCondition::ResourcePathEndsWith(".sass".into()),
-            RuleCondition::not(RuleCondition::ResourcePathEndsWith(".module.sass".into())),
-        ]),
-        RuleCondition::all(vec![
-            RuleCondition::ContentTypeStartsWith("text/css".into()),
-            RuleCondition::not(RuleCondition::ContentTypeStartsWith(
-                "text/css+module".into(),
-            )),
-        ]),
-    ])
-}
-fn module_styles_rule_condition() -> RuleCondition {
-    RuleCondition::any(vec![
-        RuleCondition::ResourcePathEndsWith(".module.css".into()),
-        RuleCondition::ResourcePathEndsWith(".module.scss".into()),
-        RuleCondition::ResourcePathEndsWith(".module.sass".into()),
-        RuleCondition::ContentTypeStartsWith("text/css+module".into()),
-    ])
-}
 impl AppProject {
     pub fn client_transition_name() -> RcStr {
         rcstr!("next-ecmascript-client-reference")
@@ -406,10 +375,10 @@ impl AppProject {
             transition_rules: vec![
                 // Mark as client reference (and exclude from RSC chunking) the edge from the
                 // CSS Module to the actual CSS
-                TransitionRule::new_internal(
+                TransitionRule::new(
                     RuleCondition::all(vec![
                         RuleCondition::ReferenceType(ReferenceType::Css(
-                            CssReferenceSubType::Internal,
+                            CssReferenceSubType::Inner,
                         )),
                         module_styles_rule_condition(),
                     ]),
@@ -678,6 +647,18 @@ impl AppProject {
         Ok(ModuleAssetContext::new(
             TransitionOptions {
                 named_transitions: transitions,
+                transition_rules: vec![
+                    // Change context, this is used to determine the list of CSS module classes.
+                    TransitionRule::new(
+                        RuleCondition::all(vec![
+                            RuleCondition::ReferenceType(ReferenceType::Css(
+                                CssReferenceSubType::Analyze,
+                            )),
+                            module_styles_rule_condition(),
+                        ]),
+                        ResolvedVc::upcast(self.client_transition().to_resolved().await?),
+                    ),
+                ],
                 ..Default::default()
             }
             .cell(),
@@ -738,6 +719,18 @@ impl AppProject {
         Ok(ModuleAssetContext::new(
             TransitionOptions {
                 named_transitions: transitions,
+                transition_rules: vec![
+                    // Change context, this is used to determine the list of CSS module classes.
+                    TransitionRule::new(
+                        RuleCondition::all(vec![
+                            RuleCondition::ReferenceType(ReferenceType::Css(
+                                CssReferenceSubType::Analyze,
+                            )),
+                            module_styles_rule_condition(),
+                        ]),
+                        ResolvedVc::upcast(self.client_transition().to_resolved().await?),
+                    ),
+                ],
                 ..Default::default()
             }
             .cell(),

crates/next-core/src/next_client/context.rs
@@ -64,7 +64,10 @@ use crate::{
         get_decorators_transform_options, get_jsx_transform_options,
         get_typescript_transform_options,
     },
-    util::{OptionEnvMap, defines, foreign_code_context_condition, internal_assets_conditions},
+    util::{
+        OptionEnvMap, defines, foreign_code_context_condition, internal_assets_conditions,
+        module_styles_rule_condition,
+    },
 };
 
 #[turbo_tasks::function]
@@ -316,6 +319,7 @@ pub async fn get_client_module_options_context(
         },
         css: CssOptionsContext {
             source_maps,
+            module_css_condition: Some(module_styles_rule_condition()),
             ..Default::default()
         },
         environment: Some(env),

crates/next-core/src/next_server/context.rs
@@ -75,6 +75,7 @@ use crate::{
     util::{
         NextRuntime, OptionEnvMap, defines, foreign_code_context_condition,
         get_transpiled_packages, internal_assets_conditions, load_next_js_templateon,
+        module_styles_rule_condition,
     },
 };
 
@@ -583,6 +584,7 @@ pub async fn get_server_module_options_context(
         environment: Some(environment),
         css: CssOptionsContext {
             source_maps,
+            module_css_condition: Some(module_styles_rule_condition()),
             ..Default::default()
         },
         tree_shaking_mode: tree_shaking_mode_for_user_code,

crates/next-core/src/next_server/transforms.rs
@@ -3,7 +3,7 @@ use next_custom_transforms::transforms::strip_page_exports::ExportFilter;
 use turbo_rcstr::RcStr;
 use turbo_tasks::{ResolvedVc, Vc};
 use turbopack::module_options::{ModuleRule, ModuleRuleEffect, RuleCondition};
-use turbopack_core::reference_type::{ReferenceType, UrlReferenceSubType};
+use turbopack_core::reference_type::{CssReferenceSubType, ReferenceType, UrlReferenceSubType};
 
 use crate::{
     mode::NextMode,
@@ -21,7 +21,7 @@ use crate::{
         next_page_static_info::get_next_page_static_info_assert_rule,
         next_pure::get_next_pure_rule, server_actions::ActionsTransform,
     },
-    util::NextRuntime,
+    util::{NextRuntime, module_styles_rule_condition, styles_rule_condition},
 };
 
 /// Returns a list of module rules which apply server-side, Next.js-specific
@@ -51,35 +51,18 @@ pub async fn get_next_server_transforms_rules(
 
     if !matches!(context_ty, ServerContextType::AppRSC { .. }) {
         rules.extend([
-            // Ignore the internal ModuleCssAsset -> CssModuleAsset references
-            // The CSS Module module itself is still needed for class names
-            ModuleRule::new_internal(
-                RuleCondition::any(vec![
-                    RuleCondition::ResourcePathEndsWith(".module.css".into()),
-                    RuleCondition::ContentTypeStartsWith("text/css+module".into()),
-                ]),
-                vec![ModuleRuleEffect::Ignore],
-            ),
-        ]);
-        rules.extend([
-            // Ignore all non-module CSS references
+            // Ignore the inner ModuleCssAsset -> CssModuleAsset references
+            // The CSS Module module itself (and the Analyze reference) is still needed to generate
+            // the class names object.
             ModuleRule::new(
-                RuleCondition::any(vec![
-                    RuleCondition::all(vec![
-                        RuleCondition::ResourcePathEndsWith(".css".into()),
-                        RuleCondition::not(RuleCondition::ResourcePathEndsWith(
-                            ".module.css".into(),
-                        )),
-                    ]),
-                    RuleCondition::all(vec![
-                        RuleCondition::ContentTypeStartsWith("text/css".into()),
-                        RuleCondition::not(RuleCondition::ContentTypeStartsWith(
-                            "text/css+module".into(),
-                        )),
-                    ]),
+                RuleCondition::all(vec![
+                    RuleCondition::ReferenceType(ReferenceType::Css(CssReferenceSubType::Inner)),
+                    module_styles_rule_condition(),
                 ]),
                 vec![ModuleRuleEffect::Ignore],
             ),
+            // Ignore all non-module CSS references
+            ModuleRule::new(styles_rule_condition(), vec![ModuleRuleEffect::Ignore]),
         ]);
     }
 

commit 333489d8dbfacfbe3c91b3f8945503192a93ace8

Turbopack: fix postcss in RSC CSS (#82554)
### What?

make sure to apply PostCSS on .module.css that is requested from RSC.

Fixes a regression from #82448

Closes #82545

turbopack/crates/turbopack/src/module_options/mod.rs
@@ -162,15 +162,17 @@ impl ModuleOptions {
         // only then be processed with Webpack/PostCSS).
         //
         // Note that this is not an exhaustive condition for PostCSS/Webpack, but excludes certain
-        // cases, so it should be added conjunctively together with the `module_css_condition` rule.
+        // cases, so it should be added conjunctively together with CSS Module rule.
         //
         // If module css, then only when (Inner or Analyze or Compose)
         // <=> (not (module css)) or (Inner or Analyzer or Compose)
-        let module_css_external_transform_conditions = vec![
+        //
+        // So only if this is not a CSS module, or one of the special reference type constraints.
+        let module_css_external_transform_conditions = RuleCondition::Any(vec![
+            RuleCondition::not(module_css_condition.clone()),
             RuleCondition::ReferenceType(ReferenceType::Css(CssReferenceSubType::Inner)),
             RuleCondition::ReferenceType(ReferenceType::Css(CssReferenceSubType::Analyze)),
-            RuleCondition::ReferenceType(ReferenceType::Css(CssReferenceSubType::Compose)),
-        ];
+        ]);
 
         let mut ts_preprocess = vec![];
         let mut ecma_preprocess = vec![];
@@ -505,22 +507,14 @@ impl ModuleOptions {
                 };
 
                 rules.push(ModuleRule::new(
-                    RuleCondition::Any(vec![
-                        RuleCondition::All(vec![
-                            RuleCondition::Any(vec![
-                                RuleCondition::ResourcePathEndsWith(".css".to_string()),
-                                RuleCondition::ContentTypeStartsWith("text/css".to_string()),
-                            ]),
-                            RuleCondition::not(module_css_condition.clone()),
+                    RuleCondition::All(vec![
+                        RuleCondition::Any(vec![
+                            // Both CSS and CSS Modules
+                            RuleCondition::ResourcePathEndsWith(".css".to_string()),
+                            RuleCondition::ContentTypeStartsWith("text/css".to_string()),
+                            module_css_condition.clone(),
                         ]),
-                        RuleCondition::All(
-                            [
-                                vec![module_css_condition.clone()],
-                                // see comment on module_css_external_transform_conditions
-                                module_css_external_transform_conditions.clone(),
-                            ]
-                            .concat(),
-                        ),
+                        module_css_external_transform_conditions.clone(),
                     ]),
                     vec![ModuleRuleEffect::SourceTransforms(ResolvedVc::cell(vec![
                         ResolvedVc::upcast(
@@ -703,14 +697,7 @@ impl ModuleOptions {
                             RuleCondition::ResourceBasePathGlob(Glob::new(key.clone()).await?)
                         },
                         RuleCondition::not(RuleCondition::ResourceIsVirtualSource),
-                        // see comment on module_css_external_transform_conditions
-                        RuleCondition::Any(
-                            [
-                                vec![RuleCondition::not(module_css_condition.clone())],
-                                module_css_external_transform_conditions.clone(),
-                            ]
-                            .concat(),
-                        ),
+                        module_css_external_transform_conditions.clone(),
                     ]),
                     vec![ModuleRuleEffect::SourceTransforms(ResolvedVc::cell(vec![
                         ResolvedVc::upcast(

test/e2e/app-dir/css-modules-rsc-postcss/app/layout.tsx
@@ -1,0 +1,8 @@
+import { ReactNode } from 'react'
+export default function Root({ children }: { children: ReactNode }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
+}

test/e2e/app-dir/css-modules-rsc-postcss/app/page.module.css
@@ -1,0 +1,5 @@
+:not(html) {
+  .main {
+    color: red;
+  }
+}

test/e2e/app-dir/css-modules-rsc-postcss/app/page.tsx
@@ -1,0 +1,5 @@
+import styles from './page.module.css'
+
+export default function Page() {
+  return <p className={styles.main}>hello world</p>
+}

test/e2e/app-dir/css-modules-rsc-postcss/css-modules-rsc-postcss.test.ts
@@ -1,0 +1,17 @@
+import { nextTestSetup } from 'e2e-utils'
+
+describe('css-modules-rsc-postcss', () => {
+  const { next } = nextTestSetup({
+    files: __dirname,
+    dependencies: {
+      'postcss-nested': '4.2.1',
+    },
+  })
+
+  it('should compile successfully and apply the correct styles', async () => {
+    const browser = await next.browser('/')
+    expect(await browser.elementByCss('p').getComputedCss('color')).toBe(
+      'rgb(0, 128, 0)'
+    )
+  })
+})

test/e2e/app-dir/css-modules-rsc-postcss/next.config.js
@@ -1,0 +1,6 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {}
+
+module.exports = nextConfig

test/e2e/app-dir/css-modules-rsc-postcss/plugin.js
@@ -1,0 +1,12 @@
+const plugin = () => {
+  return {
+    postcssPlugin: 'color-change',
+    Declaration: {
+      color(prop) {
+        prop.value = 'green'
+      },
+    },
+  }
+}
+plugin.postcss = true
+module.exports = plugin

test/e2e/app-dir/css-modules-rsc-postcss/postcss.config.js
@@ -1,0 +1,3 @@
+module.exports = {
+  plugins: ['postcss-nested', require.resolve('./plugin')],
+}