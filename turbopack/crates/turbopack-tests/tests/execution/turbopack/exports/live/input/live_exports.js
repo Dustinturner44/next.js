acf629ef7acf48d2efd225db2266d9ba41335269
Luke Sandberg <lukeisandberg@gmail.com>
2025-08-15 14:36:56 -0700

    Add a test for a free var and fix a bug

--- turbopack/crates/turbopack-tests/tests/execution/turbopack/exports/live/input/live_exports.js
+++ turbopack/crates/turbopack-tests/tests/execution/turbopack/exports/live/input/live_exports.js
@@ -3,6 +3,7 @@ let foo = 'foo'
 function setFoo(n) {
   foo = n
 }
+// biome-ignore lint/style/useConst: proving we track assignments, not declkinds
 let neverMutated = 'neverMutated'
 const obviouslyneverMutated = 'obviouslyneverMutated'
 export { foo, setFoo, neverMutated, obviouslyneverMutated }
@@ -15,3 +16,6 @@ export function bar() {
 export function setBar(b) {
   bar = b
 }
+
+// wild stuff
+export { globalThis as g }
