39c3cd625d621956ad714b3285714bcc2be1ad9e
yiminghe <yiminghe@gmail.com>
2025-09-04 00:33:35 +0800

    fix: ensure onRequestError is invoked when otel enabled (#83343)
    Co-authored-by: Jiachi Liu <inbox@huozhi.im>

--- packages/next/src/build/templates/app-page.ts
+++ packages/next/src/build/templates/app-page.ts
@@ -1244,8 +1244,7 @@ export async function handler(
       )
     }
   } catch (err) {
-    // if we aren't wrapped by base-server handle here
-    if (!activeSpan && !(err instanceof NoFallbackError)) {
+    if (!(err instanceof NoFallbackError)) {
       await routeModule.onRequestError(
         req,
         err,
