22c5a06d8d312d29c4f7705abe4569d52f809af3
Jiwon Choi <devjiwonchoi@gmail.com>
2025-10-08 22:35:27 +0200

    [Breaking] Enable `experimental.isolatedDevBuild` by default (#84561)
    ### Why?
    
    This PR cleans up the incremental rollout of enabling `isolatedDevBuild`
    throughout the CI, as it required many test changes (x-ref:
    https://github.com/vercel/next.js/pull/84043).
    
    - https://github.com/vercel/next.js/pull/84556
    - https://github.com/vercel/next.js/pull/84558
    - https://github.com/vercel/next.js/pull/84559
    - https://github.com/vercel/next.js/pull/84562
    
    This PR enables `experimental.isolatedDevBuild` by default so that users
    can run `next dev` and `next build` in the same directory, and they
    would not conflict. This occasionally happens when developing with AI
    tools, where you have a running dev server, but the Agent runs `next
    build`.
    
    ### How?
    
    - Removed `__NEXT_EXPERIMENTAL_ISOLATED_DEV_BUILD` flag and its usage
    from CI and tests
    - Updated tests that were not caught via rollout (e.g., missed out on
    test-*-windows)
    - Moved `isolatedDevBuild` option out of experimental
    - Added Telemetry
    
    Will follow up with docs.

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -2731,6 +2731,10 @@ export default async function build(
           invocationCount: config.experimental.ppr ? 1 : 0,
         },
         {
+          featureName: 'experimental/isolatedDevBuild',
+          invocationCount: config.experimental.isolatedDevBuild ? 1 : 0,
+        },
+        {
           featureName: 'turbopackFileSystemCache',
           invocationCount: isFileSystemCacheEnabledForBuild(config) ? 1 : 0,
         },
