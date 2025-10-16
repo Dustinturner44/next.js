8e3102cf1db087bdca69db84182d0d2050f7c085
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-10-16 10:57:56 -0700

    reproduction

--- bench/app-router-server/app/page.js
+++ bench/app-router-server/app/page.js
@@ -1,4 +1,10 @@
-import(`@/${globalThis.type}/zh-Hant/${globalThis.processedSlugPath}/page.ts`)
+import(`@/${globalThis.type}/${globalThis.processedSlugPath}`)
+
+// globalThis.docs = 'docs'
+// console.trace(require(`@/${globalThis.docs}/foo`))
+
+// globalThis.foo = 'foo'
+// console.trace(require(`@/docs/${globalThis.foo}`))
 
 export default function page() {
   return <div>hello</div>
