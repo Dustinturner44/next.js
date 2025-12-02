Dustinturner44/next.js

commit 3f4db01dbc1921d366a125a298850d5afaee88a7

[Cache Components] Discriminate static shell validation errors by type (#85747)
Prior to this change any "hole" in a prerender that would block the
shell was considered an error and you would be presented with a very
generic message explaining all the different ways you could have failed
this validation check.

With this change we use a new technique to validate the static shell
which can now tell the difference between waiting on uncached data or
runtime data. It also improves the heuristics around generateMetadata
and generateViewport errors.

Added new error pages for runtime sync IO and ensure we only validate
sync IO after runtime data if the page will be validating runtime
prefetches.

Restored the validation on HMR update so you can get feedback after
saving a new file.

---

We've also discovered that hanging inputs are not handled correctly.
Fixing this is non-trivial and will be done in a follow-up, so for now,
we're disabling the failing tests.

---------

Co-authored-by: Josh Story <story@hey.com>
Co-authored-by: Hendrik Liebau <mail@hendrik-liebau.de>

packages/next/src/server/app-render/staged-rendering.ts
@@ -2,20 +2,35 @@ import { InvariantError } from '../../shared/lib/invariant-error'
 import { createPromiseWithResolvers } from '../../shared/lib/promise-with-resolvers'
 
 export enum RenderStage {
-  Static = 1,
-  Runtime = 2,
-  Dynamic = 3,
+  Before = 1,
+  Static = 2,
+  Runtime = 3,
+  Dynamic = 4,
+  Abandoned = 5,
 }
 
 export type NonStaticRenderStage = RenderStage.Runtime | RenderStage.Dynamic
 
 export class StagedRenderingController {
-  currentStage: RenderStage = RenderStage.Static
+  currentStage: RenderStage = RenderStage.Before
+
+  staticInterruptReason: Error | null = null
+  runtimeInterruptReason: Error | null = null
+  staticStageEndTime: number = Infinity
+  runtimeStageEndTime: number = Infinity
+
+  private runtimeStageListeners: Array<() => void> = []
+  private dynamicStageListeners: Array<() => void> = []
 
   private runtimeStagePromise = createPromiseWithResolvers<void>()
   private dynamicStagePromise = createPromiseWithResolvers<void>()
 
-  constructor(private abortSignal: AbortSignal | null = null) {
+  private mayAbandon: boolean = false
+
+  constructor(
+    private abortSignal: AbortSignal | null = null,
+    private hasRuntimePrefetch: boolean
+  ) {
     if (abortSignal) {
       abortSignal.addEventListener(
         'abort',
@@ -32,24 +47,173 @@ export class StagedRenderingController {
         },
         { once: true }
       )
+
+      this.mayAbandon = true
     }
   }
 
-  advanceStage(stage: NonStaticRenderStage) {
-    // If we're already at the target stage or beyond, do nothing.
-    // (this can happen e.g. if sync IO advanced us to the dynamic stage)
+  onStage(stage: NonStaticRenderStage, callback: () => void) {
     if (this.currentStage >= stage) {
+      callback()
+    } else if (stage === RenderStage.Runtime) {
+      this.runtimeStageListeners.push(callback)
+    } else if (stage === RenderStage.Dynamic) {
+      this.dynamicStageListeners.push(callback)
+    } else {
+      // This should never happen
+      throw new InvariantError(`Invalid render stage: ${stage}`)
+    }
+  }
+
+  canSyncInterrupt() {
+    // If we haven't started the render yet, it can't be interrupted.
+    if (this.currentStage === RenderStage.Before) {
+      return false
+    }
+
+    const boundaryStage = this.hasRuntimePrefetch
+      ? RenderStage.Dynamic
+      : RenderStage.Runtime
+    return this.currentStage < boundaryStage
+  }
+
+  syncInterruptCurrentStageWithReason(reason: Error) {
+    if (this.currentStage === RenderStage.Before) {
       return
     }
+
+    // If Sync IO occurs during the initial (abandonable) render, we'll retry it,
+    // so we want a slightly different flow.
+    // See the implementation of `abandonRenderImpl` for more explanation.
+    if (this.mayAbandon) {
+      return this.abandonRenderImpl()
+    }
+
+    // If we're in the final render, we cannot abandon it. We need to advance to the Dynamic stage
+    // and capture the interruption reason.
+    switch (this.currentStage) {
+      case RenderStage.Static: {
+        this.staticInterruptReason = reason
+        this.advanceStage(RenderStage.Dynamic)
+        return
+      }
+      case RenderStage.Runtime: {
+        // We only error for Sync IO in the runtime stage if the route
+        // is configured to use runtime prefetching.
+        // We do this to reflect the fact that during a runtime prefetch,
+        // Sync IO aborts aborts the render.
+        // Note that `canSyncInterrupt` should prevent us from getting here at all
+        // if runtime prefetching isn't enabled.
+        if (this.hasRuntimePrefetch) {
+          this.runtimeInterruptReason = reason
+          this.advanceStage(RenderStage.Dynamic)
+        }
+        return
+      }
+      case RenderStage.Dynamic:
+      case RenderStage.Abandoned:
+      default:
+    }
+  }
+
+  getStaticInterruptReason() {
+    return this.staticInterruptReason
+  }
+
+  getRuntimeInterruptReason() {
+    return this.runtimeInterruptReason
+  }
+
+  getStaticStageEndTime() {
+    return this.staticStageEndTime
+  }
+
+  getRuntimeStageEndTime() {
+    return this.runtimeStageEndTime
+  }
+
+  abandonRender() {
+    if (!this.mayAbandon) {
+      throw new InvariantError(
+        '`abandonRender` called on a stage controller that cannot be abandoned.'
+      )
+    }
+
+    this.abandonRenderImpl()
+  }
+
+  private abandonRenderImpl() {
+    // In staged rendering, only the initial render is abandonable.
+    // We can abandon the initial render if
+    //   1. We notice a cache miss, and need to wait for caches to fill
+    //   2. A sync IO error occurs, and the render should be interrupted
+    //      (this might be a lazy intitialization of a module,
+    //       so we still want to restart in this case and see if it still occurs)
+    // In either case, we'll be doing another render after this one,
+    // so we only want to unblock the Runtime stage, not Dynamic, because
+    // unblocking the dynamic stage would likely lead to wasted (uncached) IO.
+    const { currentStage } = this
+    switch (currentStage) {
+      case RenderStage.Static: {
+        this.currentStage = RenderStage.Abandoned
+        this.resolveRuntimeStage()
+        return
+      }
+      case RenderStage.Runtime: {
+        this.currentStage = RenderStage.Abandoned
+        return
+      }
+      case RenderStage.Dynamic:
+      case RenderStage.Before:
+      case RenderStage.Abandoned:
+        break
+      default: {
+        currentStage satisfies never
+      }
+    }
+  }
+
+  advanceStage(
+    stage: RenderStage.Static | RenderStage.Runtime | RenderStage.Dynamic
+  ) {
+    // If we're already at the target stage or beyond, do nothing.
+    // (this can happen e.g. if sync IO advanced us to the dynamic stage)
+    if (stage <= this.currentStage) {
+      return
+    }
+
+    let currentStage = this.currentStage
     this.currentStage = stage
-    // Note that we might be going directly from Static to Dynamic,
-    // so we need to resolve the runtime stage as well.
-    if (stage >= RenderStage.Runtime) {
-      this.runtimeStagePromise.resolve()
+
+    if (currentStage < RenderStage.Runtime && stage >= RenderStage.Runtime) {
+      this.staticStageEndTime = performance.now() + performance.timeOrigin
+      this.resolveRuntimeStage()
     }
-    if (stage >= RenderStage.Dynamic) {
-      this.dynamicStagePromise.resolve()
+    if (currentStage < RenderStage.Dynamic && stage >= RenderStage.Dynamic) {
+      this.runtimeStageEndTime = performance.now() + performance.timeOrigin
+      this.resolveDynamicStage()
+      return
     }
+  }
+
+  /** Fire the `onStage` listeners for the runtime stage and unblock any promises waiting for it. */
+  private resolveRuntimeStage() {
+    const runtimeListeners = this.runtimeStageListeners
+    for (let i = 0; i < runtimeListeners.length; i++) {
+      runtimeListeners[i]()
+    }
+    runtimeListeners.length = 0
+    this.runtimeStagePromise.resolve()
+  }
+
+  /** Fire the `onStage` listeners for the dynamic stage and unblock any promises waiting for it. */
+  private resolveDynamicStage() {
+    const dynamicListeners = this.dynamicStageListeners
+    for (let i = 0; i < dynamicListeners.length; i++) {
+      dynamicListeners[i]()
+    }
+    dynamicListeners.length = 0
+    this.dynamicStagePromise.resolve()
   }
 
   private getStagePromise(stage: NonStaticRenderStage): Promise<void> {

test/development/app-dir/cache-components-dev-warmup/fixtures/with-prefetch-config/app/page.tsx
@@ -20,6 +20,12 @@ export default function Page() {
         <li>
           <Link href="/apis/123">/apis/123</Link>
         </li>
+        <li>
+          <Link href="/sync-io/static">/sync-io/static</Link>
+        </li>
+        <li>
+          <Link href="/sync-io/runtime">/sync-io/runtime</Link>
+        </li>
       </ul>
     </main>
   )

test/development/app-dir/cache-components-dev-warmup/fixtures/with-prefetch-config/app/sync-io/runtime/page.tsx
@@ -1,0 +1,31 @@
+import { Suspense } from 'react'
+import { CachedData, getCachedData } from '../../data-fetching'
+import { cookies } from 'next/headers'
+
+export const unstable_prefetch = { mode: 'runtime', samples: [{}] }
+
+const CACHE_KEY = __dirname + '/__PAGE__'
+
+export default async function Page() {
+  return (
+    <main>
+      <h1>Sync IO - runtime stage</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Runtime />
+      </Suspense>
+    </main>
+  )
+}
+
+async function Runtime() {
+  await getCachedData(CACHE_KEY + '-1')
+  console.log(`after first cache`)
+
+  await cookies()
+  console.log(`after cookies`)
+
+  Date.now()
+  console.log(`after sync io`)
+
+  return <CachedData label="page" cacheKey={CACHE_KEY} />
+}

test/development/app-dir/cache-components-dev-warmup/fixtures/with-prefetch-config/app/sync-io/static/page.tsx
@@ -1,0 +1,20 @@
+import { CachedData, getCachedData } from '../../data-fetching'
+
+export const unstable_prefetch = { mode: 'runtime', samples: [{}] }
+
+const CACHE_KEY = __dirname + '/__PAGE__'
+
+export default async function Page() {
+  await getCachedData(CACHE_KEY + '-1')
+  console.log(`after first cache`)
+
+  Date.now()
+  console.log(`after sync io`)
+
+  return (
+    <main>
+      <h1>Sync IO - static stage</h1>
+      <CachedData label="page" cacheKey={CACHE_KEY} />
+    </main>
+  )
+}

test/development/app-dir/cache-components-dev-warmup/fixtures/without-prefetch-config/app/page.tsx
@@ -20,6 +20,12 @@ export default function Page() {
         <li>
           <Link href="/apis/123">/apis/123</Link>
         </li>
+        <li>
+          <Link href="/sync-io/static">/sync-io/static</Link>
+        </li>
+        <li>
+          <Link href="/sync-io/runtime">/sync-io/runtime</Link>
+        </li>
       </ul>
     </main>
   )

test/development/app-dir/cache-components-dev-warmup/fixtures/without-prefetch-config/app/sync-io/runtime/page.tsx
@@ -1,0 +1,29 @@
+import { Suspense } from 'react'
+import { CachedData, getCachedData } from '../../data-fetching'
+import { cookies } from 'next/headers'
+
+const CACHE_KEY = __dirname + '/__PAGE__'
+
+export default async function Page() {
+  return (
+    <main>
+      <h1>Sync IO - runtime stage</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Runtime />
+      </Suspense>
+    </main>
+  )
+}
+
+async function Runtime() {
+  await getCachedData(CACHE_KEY + '-1')
+  console.log(`after first cache`)
+
+  await cookies()
+  console.log(`after cookies`)
+
+  Date.now()
+  console.log(`after sync io`)
+
+  return <CachedData label="page" cacheKey={CACHE_KEY} />
+}

test/development/app-dir/cache-components-dev-warmup/fixtures/without-prefetch-config/app/sync-io/static/page.tsx
@@ -1,0 +1,18 @@
+import { CachedData, getCachedData } from '../../data-fetching'
+
+const CACHE_KEY = __dirname + '/__PAGE__'
+
+export default async function Page() {
+  await getCachedData(CACHE_KEY + '-1')
+  console.log(`after first cache`)
+
+  Date.now()
+  console.log(`after sync io`)
+
+  return (
+    <main>
+      <h1>Sync IO - static stage</h1>
+      <CachedData label="page" cacheKey={CACHE_KEY} />
+    </main>
+  )
+}

test/development/app-dir/cache-components-dev-warmup/fixtures/without-prefetch-config/next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  experimental: {
-    cacheComponents: true,
-  },
+  cacheComponents: true,
 }
 
 export default nextConfig

commit 1660d3966a99371a57b3d832522caa55b42bc189

[CC] Fix hanging dynamic promise when abandoning render (#86690)
Fixes #86662

The bug was introduced in #85747, where we added
`RenderStage.Abandoned`. We forgot to update the logic that rejects
promises on abort, so if a render was abandoned (i.e. restarted due to a
cache miss), then promises waiting for the dynamic stage would hang
forever.

This is not generally observable unless the promise is persisted/cached
in userspace. Userspace caches are problematic in Cache Components and
we discourage their usage, but the test app included here should still
work as expected (i.e. without hanging forever while rendering).

packages/next/src/server/app-render/staged-rendering.ts
@@ -40,7 +40,10 @@ export class StagedRenderingController {
             this.runtimeStagePromise.promise.catch(ignoreReject) // avoid unhandled rejections
             this.runtimeStagePromise.reject(reason)
           }
-          if (this.currentStage < RenderStage.Dynamic) {
+          if (
+            this.currentStage < RenderStage.Dynamic ||
+            this.currentStage === RenderStage.Abandoned
+          ) {
             this.dynamicStagePromise.promise.catch(ignoreReject) // avoid unhandled rejections
             this.dynamicStagePromise.reject(reason)
           }

test/development/app-dir/cache-components-reused-promise/app/layout.tsx
@@ -1,0 +1,7 @@
+export default async function Layout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  )
+}

test/development/app-dir/cache-components-reused-promise/app/page.tsx
@@ -1,0 +1,71 @@
+// This repros a bug from https://github.com/vercel/next.js/issues/86662.
+// The bug from occurs if we have:
+// - a cache (which requires warming = a restart in dev)
+// - a dynamic function (blocked until the dynamic stage, like an uncached fetch)
+//   that dedupes concurrent calls. note that that's not quite the same as caching it,
+//   because it gets dropped after finishing.
+
+import { Suspense } from 'react'
+
+export default async function Page() {
+  return (
+    <main>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div id="random">
+          <DynamicRandom />
+        </div>
+      </Suspense>
+    </main>
+  )
+}
+
+async function DynamicRandom() {
+  const [, random] = await Promise.all([
+    // ensure that there's a restart to warm this cache.
+    cached(),
+    // This fetch going to be blocked on the dynamic stage.
+    // That promise should be rejected when restarting.
+    // If it's not rejected (like it wasn't before the fix),
+    // it'll remain hanging, and we'll never render anything.
+    fetchDynamicRandomDeduped(),
+  ])
+  return random
+}
+
+function dedupeConcurrent<T>(func: () => Promise<T>): () => Promise<T> {
+  let pending: Promise<T> | null = null
+  return () => {
+    if (pending !== null) {
+      console.log('dedupe :: re-using pending promise')
+      return pending
+    }
+
+    console.log('dedupe :: starting')
+    const promise = func()
+    pending = promise
+
+    const clearPending = () => {
+      console.log('dedupe :: finished')
+      pending = null
+    }
+    promise.then(clearPending, clearPending)
+
+    return promise
+  }
+}
+
+const fetchDynamicRandomDeduped = dedupeConcurrent(async () => {
+  const res = await fetch(
+    'https://next-data-api-endpoint.vercel.app/api/random'
+  )
+  if (!res.ok) {
+    throw new Error(`request failed with status ${res.status}`)
+  }
+  const text = await res.text()
+  return text
+})
+
+async function cached() {
+  'use cache'
+  await new Promise((resolve) => setTimeout(resolve))
+}

test/development/app-dir/cache-components-reused-promise/cache-components-reused-promise.test.ts
@@ -1,0 +1,12 @@
+import { nextTestSetup } from 'e2e-utils'
+
+describe('cache-components-dev-warmup - reused promise', () => {
+  const { next } = nextTestSetup({
+    files: __dirname,
+  })
+
+  it('aborts dynamic promises when restarting the render', async () => {
+    const browser = await next.browser('/')
+    expect(await browser.elementByCss('#random').text()).toMatch(/\d+\.\d+/)
+  })
+})

test/development/app-dir/cache-components-reused-promise/next.config.ts
@@ -1,0 +1,7 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  cacheComponents: true,
+}
+
+export default nextConfig